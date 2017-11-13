2a8dd40fbd7184df6b0f4337cca21d27c808ae48
Owais <oakbani@folio3.com>
2017-11-13 15:05:04 +0500

    :pencil2: Added some more tests

--- tests/NotificationTests/NotificationCenterTest.php
+++ tests/NotificationTests/NotificationCenterTest.php
@@ -19,6 +19,7 @@ namespace Optimizely\Tests;
 use Exception;
 use Monolog\Logger;
 
+use Optimizely\Event\Builder\EventBuilder;
 use Optimizely\Notification\NotificationCenter;
 use Optimizely\Notification\NotificationType;
 use Optimizely\Logger\NoOpLogger;
@@ -64,36 +65,298 @@ class NotificationCenterTest extends \PHPUnit_Framework_TestCase
 
     public function testAddNotificationWithValidTypeAndCallback(){
         $notificationType = NotificationType::DECISON;
-        $notificationsLen = sizeof($this->notificationCenterObj->getNotifications()[$notificationType]);
 
-        // should add, log and return notification ID when a plain function is passed as an argument
+        // ensure that notifications length is zero
+        $this->notificationCenterObj->clearAllNotifications();
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[$notificationType])
+        );
+
+        //  ===== should add, log and return notification ID when a plain function is passed as an argument =====
         $simple_method = function(){};
         $this->loggerMock->expects($this->at(0))
             ->method('log')
             ->with(Logger::INFO, "Callback added for notification type '{$notificationType}'.");
         $this->assertSame(
             1,
-            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, $simple_method)
+            $this->notificationCenterObj->addNotificationListener($notificationType, $simple_method)
         );
+        // verify that notifications length has incremented by 1
         $this->assertSame(
-            ++$notificationsLen,
+            1,
             sizeof($this->notificationCenterObj->getNotifications()[$notificationType])
         );
 
-        // should add, log and return notification ID when a anonymous function is passed as an argument
+        // ===== should add, log and return notification ID when an anonymous function is passed as an argument =====
         $this->loggerMock->expects($this->at(0))
             ->method('log')
             ->with(Logger::INFO, "Callback added for notification type '{$notificationType}'.");
         $this->assertSame(
             2,
-            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){})
+            $this->notificationCenterObj->addNotificationListener($notificationType, function(){})
         );
+         // verify that notifications length has incremented by 1
         $this->assertSame(
-            ++$notificationsLen,
+            2,
             sizeof($this->notificationCenterObj->getNotifications()[$notificationType])
         );
 
-        // should add, log and return notification ID when an object method is passed as an argument
-        // TODO on Monday
-    }    
+        // ===== should add, log and return notification ID when an object method is passed as an argument ===== 
+        $eBuilder = new EventBuilder;
+        $callbackInput = array($eBuilder, 'createImpressionEvent');
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, "Callback added for notification type '{$notificationType}'.");
+        $this->assertSame(
+            3,
+            $this->notificationCenterObj->addNotificationListener($notificationType, $callbackInput)
+        );
+         // verify that notifications length has incremented by 1
+        $this->assertSame(
+            3,
+            sizeof($this->notificationCenterObj->getNotifications()[$notificationType])
+        );
+    } 
+
+    public function testAddNotificationForMultipleNotificationTypes(){
+        // ensure that notifications length is zero for each notification type
+        $this->notificationCenterObj->clearAllNotifications();
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::TRACK])
+        );
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::FEATURE_ACCESSED])
+        );
+
+        // ===== should add, log and return notification ID when a valid callback is added for each notification type =====
+        
+         $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::DECISON));
+        $this->assertSame(
+            1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){})
+        );
+         // verify that notifications length for NotificationType::DECISON has incremented by 1
+        $this->assertSame(
+            1,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::TRACK));
+        $this->assertSame(
+            2,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::TRACK, function(){})
+        );
+         // verify that notifications length for NotificationType::TRACK has incremented by 1
+        $this->assertSame(
+            1,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::TRACK])
+        );
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::FEATURE_ACCESSED));
+        $this->assertSame(
+            3,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::FEATURE_ACCESSED, function(){})
+        );
+         // verify that notifications length for NotificationType::FEATURE_ACCESSED has incremented by 1
+        $this->assertSame(
+            1,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::FEATURE_ACCESSED])
+        );
+    }
+
+    public function testAddNotificationForMultipleCallbacksForANotificationType(){
+        // ensure that notifications length is zero for notification type
+        $this->notificationCenterObj->clearAllNotifications();
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        // ===== should add, log and return notification ID when multiple valid callbacks 
+        // are added for a single notification type =====  
+         $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::DECISON));
+        $this->assertSame(
+            1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){})
+        );
+         // verify that notifications length for NotificationType::DECISON has incremented by 1
+        $this->assertSame(
+            1,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        $this->assertSame(
+            2,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){
+                echo "HelloWorld";
+            })
+        );
+         // verify that notifications length for NotificationType::DECISON has incremented by 1
+        $this->assertSame(
+            2,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        $this->assertSame(
+            3,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){
+                $a = 1;
+            })
+        );
+         // verify that notifications length for NotificationType::DECISON has incremented by 1
+        $this->assertSame(
+            3,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+    }
+
+    public function testAddNotificationThatAlreadyAddedCallbackIsNotReAdded(){
+        // Note: anonymous methods sent with the same body will be re-added. 
+        // Only variable and object methods can be checked for duplication
+        
+        $functionToSend = function(){};
+        // ensure that notifications length is zero
+        $this->notificationCenterObj->clearAllNotifications();
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        ///////////////////////////////////////////////////////////////////////////
+        // ===== verify that a variable method with same body isn't re-added ===== //
+        ///////////////////////////////////////////////////////////////////////////
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::DECISON));
+        // verify that notification ID 1 is returned
+        $this->assertSame(
+            1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, $functionToSend)
+        );
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::DEBUG, sprintf("Callback already added for notification type '%s'.", NotificationType::DECISON));
+
+        // verify that -1 is returned when adding the same callback
+        $this->assertSame(
+            -1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, $functionToSend)
+        );
+
+        // verify that same method is added for a different notification type
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::TRACK));
+        $this->assertSame(
+            2,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::TRACK, $functionToSend)
+        );
+        
+        /////////////////////////////////////////////////////////////////////////
+        // ===== verify that an object method with same body isn't re-added ===== //
+        /////////////////////////////////////////////////////////////////////////
+        $eBuilder = new EventBuilder;
+        $callbackInput = array($eBuilder, 'createImpressionEvent');
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::DECISON));
+        $this->assertSame(
+            3,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, $callbackInput)
+        );
+
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::DEBUG, sprintf("Callback already added for notification type '%s'.", NotificationType::DECISON));
+        // verify that -1 is returned when adding the same callback
+        $this->assertSame(
+            -1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, $callbackInput)
+        );
+        // verify that same method is added for a different notification type
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::INFO, sprintf("Callback added for notification type '%s'.", NotificationType::TRACK));
+        $this->assertSame(
+            4,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::TRACK, $callbackInput)
+        );
+    }
+
+
+    public function testRemoveNotification(){
+        // ensure that notifications length is zero for each notification type
+        $this->notificationCenterObj->clearAllNotifications();
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+        $this->assertSame(
+            0,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::TRACK])
+        );
+
+        // add a callback for multiple notification types
+        $this->assertSame(
+            1,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){})
+        );
+         $this->assertSame(
+            2,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::TRACK, function(){})
+        );
+        // add another callback for NotificationType::DECISON
+        $this->assertSame(
+            3,
+            $this->notificationCenterObj->addNotificationListener(NotificationType::DECISON, function(){
+            //doSomething
+            })
+        );
+
+        // Verify that notifications length for NotificationType::DECISON is 2
+        $this->assertSame(
+            2,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::DECISON])
+        );
+
+        // Verify that notifications length for NotificationType::TRACK is 1
+         $this->assertSame(
+            1,
+            sizeof($this->notificationCenterObj->getNotifications()[NotificationType::TRACK])
+        );
+
+
+        ///////////////////////////////////////////////////////////////////////////////
+        // === Verify that no callback is removed for an invalid notification ID === //
+        ///////////////////////////////////////////////////////////////////////////////
+        $invalid_id = 4;
+        $this->loggerMock->expects($this->at(0))
+            ->method('log')
+            ->with(Logger::DEBUG, sprintf("No Callback found with notification ID '%s'.",$invalid_id));
+        $this->assertSame(
+            false,
+            $this->notificationCenterObj->removeNotificationListener($invalid_id)
+        );
+
+    }
+
 }
+
