0c33c8936325e2242a7d3fd22b021dae4d5cba8d
Rashid Siddique <rashidsp204@gmail.com>
2018-09-04 16:23:45 +0500

    perf(isValid): Adds getter to access isValid attribute

--- src/Optimizely/Optimizely.php
+++ src/Optimizely/Optimizely.php
@@ -772,6 +772,14 @@ class Optimizely
     }
 
     /**
+     * @return boolean Flag denoting if Optimizely is valid.
+     */
+    public function isValid()
+    {
+        return $this->_isValid;
+    }
+
+    /**
     * Calls Validator::validateNonEmptyString for each value in array
     * Logs for each invalid value
     *
